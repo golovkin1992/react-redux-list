1818be1660739d42ffd19bf453a05e030501f550
User <golova92-69@yandex.ru>
2019-08-14 01:38:39 +0300

    Updates

---
+++ precache-manifest.309bdf9c7b3f7713ab65d2801748a2a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2522d805e6b0d41b1d1cde449d75e843",
+    "url": "/react-redux-list/index.html"
+  },
+  {
+    "revision": "9f43a15ee03331f451bd",
+    "url": "/react-redux-list/static/css/main.edabde86.chunk.css"
+  },
+  {
+    "revision": "74b15c307d109ea38572",
+    "url": "/react-redux-list/static/js/2.df07fd41.chunk.js"
+  },
+  {
+    "revision": "9f43a15ee03331f451bd",
+    "url": "/react-redux-list/static/js/main.2557332f.chunk.js"
+  },
+  {
+    "revision": "77e198a3eb133f5e0b36",
+    "url": "/react-redux-list/static/js/runtime~main.5988db2a.js"
+  },
+  {
+    "revision": "23f4d170217d0f679269b75ac539974f",
+    "url": "/react-redux-list/static/media/582710.23f4d170.jpg"
+  }
+]);
